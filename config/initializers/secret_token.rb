395b520b50ada37b46b12b01a01db6a9e650985a
lostinwent <stein@steinw.com>
2013-07-09 15:31:20 -0400

    Generating the product model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest::Application.config.secret_token = 'f3675ebcac255aa2058a6ce898d8e933db8797ab3ed2cd97618d8888001afafd4e10bc91bfb106dcc48ceb4b8a425ab9a117b3a447546f67964fa510147af133'
