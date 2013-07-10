34661f35c4d45d7e85e005598de9906858968478
lostinwent <stein@steinw.com>
2013-07-10 13:26:35 -0400

    Add _form.html.erb as a partial templates for the forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,11 +14,27 @@ class ProductsController < ApplicationController
   def create
   	@product = Product.new(params[:product])
   	if @product.save
-  		redirect_to product_path
-  		
+  		redirect_to product_path(@product)
+  	else
+  		render :new
   	end
   end
 
   def edit
+  	@product = Product.find(params[:id])
   end
+
+  def update
+  	@product = Product.find(params[:id])
+  	if @product.update_attributes(params[:product])
+  		redirect_to product_path(@product)
+  	else
+  		render :edit
+  	end
+  end
+
+	def destory
+	  @product = Product.find(params[:id])
+	  @product.destory
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rainforest::Application.routes.draw do
-  get    'products'          => 'products#index'
-  post   'products'          => 'products#create'
-  get    'products/new'      => 'products#new'
-  get    'products/:id/edit' => 'products#edit'
-  get    'products/:id'      => 'products#show', :as 'product'
-  put    'products/:id'      => 'products#update'
-  delete 'products/:id'      => 'products#destroy'
-  root   :to                 => 'pictures#index'
+  # get    'products'          => 'products#index'
+  # post   'products'          => 'products#create'
+  # get    'products/new'      => 'products#new'
+  # get    'products/:id/edit' => 'products#edit'
+  # get    'products/:id'      => 'products#show',
+  # put    'products/:id'      => 'products#update'
+  # delete 'products/:id'      => 'products#destroy'
+  resources :products
+  root      :to                => 'products#index'
 end
+
+
+
+
+
